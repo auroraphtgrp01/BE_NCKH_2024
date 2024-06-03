9c0706a7476760de9a0e2ad5766d8caa95393ab7
Trần Nguyễn Duy Khánh <147808163+AugustusDngQt@users.noreply.github.com>
2024-06-03 14:50:42 +0700

    Command

--- src/commons/common.service.ts
+++ src/commons/common.service.ts
@@ -6,51 +6,49 @@ import { ContractAttribute, ContractAttributeInBlockchain } from '@prisma/client
 
 @Injectable()
 export class CommonService {
-   convertToTypeContractAttributesResponse(
-      contractAttributes: ContractAttribute[] | ContractAttributeInBlockchain[]
-   ): IContractAttributeResponse[] {
-      const result: IContractAttributeResponse[] = []
-      for (const contractArribute of contractAttributes) {
-         if (
-            contractArribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE ||
-            contractArribute.type === ETypeContractAttribute.CONTRACT_SIGNATURE ||
-            contractArribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_JOINED ||
-            contractArribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_RECEIVE ||
-            contractArribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_SEND ||
-            contractArribute.type === ETypeContractAttribute.TOTAL_AMOUNT
-         )
-            result.push({
-               id: contractArribute.id,
-               property: contractArribute.value,
-               value: (contractArribute as any).ContractAttributeValue.value,
-               type: contractArribute.type,
-               createdBy: contractArribute.createdBy,
-               updatedBy: contractArribute.updatedBy
-            })
-         else
-            result.push({
-               id: contractArribute.id,
-               value: contractArribute.value,
-               type: contractArribute.type,
-               createdBy: contractArribute.createdBy,
-               updatedBy: contractArribute.updatedBy
-            })
-      }
-      return result
-   }
-
-   uuidv4() {
-      return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
-         (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+  convertToTypeContractAttributesResponse(contractAttributes: ContractAttribute[]): IContractAttributeResponse[] {
+    const result: IContractAttributeResponse[] = []
+    for (const contractArribute of contractAttributes) {
+      if (
+        contractArribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE ||
+        contractArribute.type === ETypeContractAttribute.CONTRACT_SIGNATURE ||
+        contractArribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_JOINED ||
+        contractArribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_RECEIVE ||
+        contractArribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_SEND ||
+        contractArribute.type === ETypeContractAttribute.TOTAL_AMOUNT
       )
-   }
+        result.push({
+          id: contractArribute.id,
+          property: contractArribute.value,
+          value: (contractArribute as any).ContractAttributeValue.value,
+          type: contractArribute.type,
+          createdBy: contractArribute.createdBy,
+          updatedBy: contractArribute.updatedBy
+        })
+      else
+        result.push({
+          id: contractArribute.id,
+          value: contractArribute.value,
+          type: contractArribute.type,
+          createdBy: contractArribute.createdBy,
+          updatedBy: contractArribute.updatedBy
+        })
+    }
+    return result
+  }
 
-   createRandomString(length: number): string {
-      const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-      let result = ''
-      for (let i = 0; i < length; i++) {
-         result += chars.charAt(Math.floor(Math.random() * chars.length))
-      }
-      return result
-   }
+  uuidv4() {
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+    )
+  }
+
+  createRandomString(length: number): string {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let result = ''
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+    return result
+  }
 }
--- src/contract-attributes/contract-attributes.service.ts
+++ src/contract-attributes/contract-attributes.service.ts
@@ -7,9 +7,9 @@ import { ExtendedPrismaClient } from 'src/utils/prisma.extensions'
 import { IExecutor } from 'src/interfaces/executor.interface'
 import { RESPONSE_MESSAGES } from 'src/constants/responseMessage.constant'
 import {
-   IContractAttributeResponse,
-   ICreateContractAttributeRecord,
-   IDataContractAttribute
+  IContractAttributeResponse,
+  ICreateContractAttributeRecord,
+  IDataContractAttribute
 } from 'src/interfaces/contract-attribute.interface'
 import { CommonService } from 'src/commons/common.service'
 import { TemplateContractsService } from 'src/template-contracts/template-contracts.service'
@@ -19,220 +19,220 @@ import { ContractsService } from 'src/contracts/contracts.service'
 
 @Injectable()
 export class ContractAttributesService {
-   constructor(
-      @Inject('PrismaService') private prismaService: CustomPrismaService<ExtendedPrismaClient>,
-      @Inject(forwardRef(() => CommonService)) private readonly commonService: CommonService,
-      @Inject(forwardRef(() => TemplateContractsService))
-      private readonly templateContractsService: TemplateContractsService,
-      @Inject(forwardRef(() => ContractAttributeValuesService))
-      private readonly contractAttributeValueService: ContractAttributeValuesService,
-      @Inject(forwardRef(() => ContractsService))
-      private readonly contractsService: ContractsService
-   ) {}
-   async create(createContractAttributeDto: CreateContractAttributeDto, user: IUser) {
-      const { contractId, ...rest } = createContractAttributeDto
+  constructor(
+    @Inject('PrismaService') private prismaService: CustomPrismaService<ExtendedPrismaClient>,
+    @Inject(forwardRef(() => CommonService)) private readonly commonService: CommonService,
+    @Inject(forwardRef(() => TemplateContractsService))
+    private readonly templateContractsService: TemplateContractsService,
+    @Inject(forwardRef(() => ContractAttributeValuesService))
+    private readonly contractAttributeValueService: ContractAttributeValuesService,
+    @Inject(forwardRef(() => ContractsService))
+    private readonly contractsService: ContractsService
+  ) {}
+  async create(createContractAttributeDto: CreateContractAttributeDto, user: IUser) {
+    const { contractId, ...rest } = createContractAttributeDto
 
-      const createdBy: IExecutor = { id: user.id, name: user.name, email: user.email, role: user.role }
-      const data: ICreateContractAttributeRecord = { ...rest }
+    const createdBy: IExecutor = { id: user.id, name: user.name, email: user.email, role: user.role }
+    const data: ICreateContractAttributeRecord = { ...rest }
 
-      if (contractId) data.Contract = { connect: { id: contractId } }
+    if (contractId) data.Contract = { connect: { id: contractId } }
 
-      const contractAttribute = await this.prismaService.client.contractAttribute.create({
-         data: {
-            ...data,
-            createdBy,
-            updatedAt: null
-         }
-      })
-
-      const result: IContractAttributeResponse = {
-         id: contractAttribute.id,
-         value: contractAttribute.value,
-         type: contractAttribute.type,
-         createdBy
+    const contractAttribute = await this.prismaService.client.contractAttribute.create({
+      data: {
+        ...data,
+        createdBy,
+        updatedAt: null
       }
+    })
 
-      return result
-   }
+    const result: IContractAttributeResponse = {
+      id: contractAttribute.id,
+      value: contractAttribute.value,
+      type: contractAttribute.type,
+      createdBy
+    }
 
-   async createInBlockchain(createContractAttributeDto: CreateContractAttributeDto) {
-      const { contractId, ...rest } = createContractAttributeDto
+    return result
+  }
 
-      const data: ICreateContractAttributeRecord = { ...rest }
+  async createInBlockchain(createContractAttributeDto: CreateContractAttributeDto) {
+    const { contractId, ...rest } = createContractAttributeDto
 
-      if (contractId) data.Contract = { connect: { id: contractId } }
+    const data: ICreateContractAttributeRecord = { ...rest }
 
-      const contractAttribute = await this.prismaService.client.contractAttributeInBlockchain.create({
-         data: {
-            ...data
-         }
-      })
+    if (contractId) data.Contract = { connect: { id: contractId } }
 
-      const result = {
-         id: contractAttribute.id,
-         value: contractAttribute.value,
-         type: contractAttribute.type
+    const contractAttribute = await this.prismaService.client.contractAttributeInBlockchain.create({
+      data: {
+        ...data
       }
+    })
 
-      return result
-   }
+    const result = {
+      id: contractAttribute.id,
+      value: contractAttribute.value,
+      type: contractAttribute.type
+    }
 
-   async createContractAttributes(
-      createContractAttributesDto: CreateContractAttributesDto,
-      user: IUser,
-      index: number = 0
-   ) {
-      const { contractAttributes, contractId } = createContractAttributesDto
-      const contractAttributeRecords: IContractAttributeResponse[] = []
-      const createdBy: IExecutor = { id: user.id, name: user.name, email: user.email, role: user.role }
-      if (contractId && !(await this.prismaService.client.contract.findUnique({ where: { id: contractId } })))
-         throw new NotFoundException(RESPONSE_MESSAGES.CONTRACT_NOT_FOUND)
-      for (const contractAttribute of contractAttributes) {
-         if (!Object.values(ETypeContractAttribute).includes(contractAttribute.type as ETypeContractAttribute)) {
-            throw new BadRequestException(RESPONSE_MESSAGES.TYPE_CONTRACT_ATTRIBUTE_IS_NOT_VALID)
-         }
-         const data: IDataContractAttribute = {
-            value: null,
-            type: contractAttribute.type
-         }
-         if (contractId) data.contractId = contractId
-         data.index = index
-         if (
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE ||
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_SIGNATURE ||
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_JOINED ||
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_RECEIVE ||
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_SEND ||
-            contractAttribute.type === ETypeContractAttribute.TOTAL_AMOUNT
-         ) {
-            data.value = contractAttribute.property
-            const contractAttributeRecord = await this.create(data, user)
-            const contractAttributeValueRecord = await this.contractAttributeValueService.create(
-               {
-                  value: contractAttribute.value !== 'Empty' ? contractAttribute.value : '',
-                  contractAttributeId: contractAttributeRecord.id
-               },
-               user
-            )
-            const result: IContractAttributeResponse = {
-               id: contractAttributeRecord.id,
-               property: contractAttributeRecord.value,
-               value: contractAttributeValueRecord.value,
-               type: contractAttributeRecord.type,
-               createdBy
-            }
-            contractAttributeRecords.push(result)
-         } else {
-            data.value = contractAttribute.value
-            const contractAttributeRecord = await this.create(data, user)
-            contractAttributeRecords.push(contractAttributeRecord)
-         }
-         index++
+    return result
+  }
+
+  async createContractAttributes(
+    createContractAttributesDto: CreateContractAttributesDto,
+    user: IUser,
+    index: number = 0
+  ) {
+    const { contractAttributes, contractId } = createContractAttributesDto
+    const contractAttributeRecords: IContractAttributeResponse[] = []
+    const createdBy: IExecutor = { id: user.id, name: user.name, email: user.email, role: user.role }
+    if (contractId && !(await this.prismaService.client.contract.findUnique({ where: { id: contractId } })))
+      throw new NotFoundException(RESPONSE_MESSAGES.CONTRACT_NOT_FOUND)
+    for (const contractAttribute of contractAttributes) {
+      if (!Object.values(ETypeContractAttribute).includes(contractAttribute.type as ETypeContractAttribute)) {
+        throw new BadRequestException(RESPONSE_MESSAGES.TYPE_CONTRACT_ATTRIBUTE_IS_NOT_VALID)
       }
-
-      return contractAttributeRecords
-   }
-
-   async createContractAttributesInBlockchain(createContractAttributesDto: CreateContractAttributesDto) {
-      const { contractAttributes, contractId } = createContractAttributesDto
-      let index: number = 0
-      for (const contractAttribute of contractAttributes) {
-         const data: IDataContractAttribute = {
-            value: null,
-            type: contractAttribute.type
-         }
-         if (contractId) data.contractId = contractId
-         data.index = index
-         if (
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE ||
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_SIGNATURE ||
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_JOINED ||
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_RECEIVE ||
-            contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_SEND ||
-            contractAttribute.type === ETypeContractAttribute.TOTAL_AMOUNT
-         ) {
-            data.value = contractAttribute.property
-            const contractAttributeRecord = await this.createInBlockchain(data)
-            await this.contractAttributeValueService.createInBlockchain({
-               value: contractAttribute.value !== 'Empty' ? contractAttribute.value : '',
-               contractAttributeId: contractAttributeRecord.id
-            })
-         } else {
-            data.value = contractAttribute.value
-            await this.createInBlockchain(data)
-         }
-         index++
+      const data: IDataContractAttribute = {
+        value: null,
+        type: contractAttribute.type
       }
-   }
+      if (contractId) data.contractId = contractId
+      data.index = index
+      if (
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE ||
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_SIGNATURE ||
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_JOINED ||
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_RECEIVE ||
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_SEND ||
+        contractAttribute.type === ETypeContractAttribute.TOTAL_AMOUNT
+      ) {
+        data.value = contractAttribute.property
+        const contractAttributeRecord = await this.create(data, user)
+        const contractAttributeValueRecord = await this.contractAttributeValueService.create(
+          {
+            value: contractAttribute.value !== 'Empty' ? contractAttribute.value : '',
+            contractAttributeId: contractAttributeRecord.id
+          },
+          user
+        )
+        const result: IContractAttributeResponse = {
+          id: contractAttributeRecord.id,
+          property: contractAttributeRecord.value,
+          value: contractAttributeValueRecord.value,
+          type: contractAttributeRecord.type,
+          createdBy
+        }
+        contractAttributeRecords.push(result)
+      } else {
+        data.value = contractAttribute.value
+        const contractAttributeRecord = await this.create(data, user)
+        contractAttributeRecords.push(contractAttributeRecord)
+      }
+      index++
+    }
 
-   async findAllByContractId(contractId: string): Promise<IContractAttributeResponse[]> {
-      const contractAttributes = await this.prismaService.client.contractAttribute
-         .findMany({
-            where: { contractId },
-            include: { ContractAttributeValue: true },
-            orderBy: {
-               index: 'asc'
-            }
-         })
-         .then((contractAttributes) => this.commonService.convertToTypeContractAttributesResponse(contractAttributes))
+    return contractAttributeRecords
+  }
 
-      return contractAttributes
-   }
+  async createContractAttributesInBlockchain(createContractAttributesDto: CreateContractAttributesDto) {
+    const { contractAttributes, contractId } = createContractAttributesDto
+    let index: number = 0
+    for (const contractAttribute of contractAttributes) {
+      const data: IDataContractAttribute = {
+        value: null,
+        type: contractAttribute.type
+      }
+      if (contractId) data.contractId = contractId
+      data.index = index
+      if (
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE ||
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_SIGNATURE ||
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_JOINED ||
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_RECEIVE ||
+        contractAttribute.type === ETypeContractAttribute.CONTRACT_ATTRIBUTE_PARTY_ADDRESS_WALLET_SEND ||
+        contractAttribute.type === ETypeContractAttribute.TOTAL_AMOUNT
+      ) {
+        data.value = contractAttribute.property
+        const contractAttributeRecord = await this.createInBlockchain(data)
+        await this.contractAttributeValueService.createInBlockchain({
+          value: contractAttribute.value !== 'Empty' ? contractAttribute.value : '',
+          contractAttributeId: contractAttributeRecord.id
+        })
+      } else {
+        data.value = contractAttribute.value
+        await this.createInBlockchain(data)
+      }
+      index++
+    }
+  }
 
-   async findAllInBlockchainByContractId(contractId: string): Promise<IContractAttributeResponse[]> {
-      const contractAttributes = await this.prismaService.client.contractAttributeInBlockchain
-         .findMany({
-            where: { contractId },
-            include: { ContractAttributeValueInBlockchain: true },
-            orderBy: {
-               index: 'asc'
-            }
-         })
-         .then((contractAttributes) => this.commonService.convertToTypeContractAttributesResponse(contractAttributes))
-
-      return contractAttributes
-   }
-
-   async findAllByTemplateId(templateContractId: string) {
-      const templateContract = await this.templateContractsService.findOneById(templateContractId)
-      const contractAttributes = await Promise.all(
-         templateContract.contractAttributes.map(async (item) => {
-            const contractAttribute = await this.prismaService.client.contractAttribute.findFirst({
-               where: { id: item },
-               include: { ContractAttributeValue: true }
-            })
-            return contractAttribute
-         })
-      )
-      const result = await Promise.all(this.commonService.convertToTypeContractAttributesResponse(contractAttributes))
-      return result
-   }
-
-   async findOneById(id: string) {
-      return await this.prismaService.client.contractAttribute.findUnique({ where: { id } })
-   }
-
-   async findOne(payload: string) {
-      const contractAttribute = await this.prismaService.client.contractAttribute.findFirst({
-         where: { value: payload }
+  async findAllByContractId(contractId: string): Promise<IContractAttributeResponse[]> {
+    const contractAttributes = await this.prismaService.client.contractAttribute
+      .findMany({
+        where: { contractId },
+        include: { ContractAttributeValue: true },
+        orderBy: {
+          index: 'asc'
+        }
       })
-      return contractAttribute
-   }
+      .then((contractAttributes) => this.commonService.convertToTypeContractAttributesResponse(contractAttributes))
 
-   async update(updateContractAttributeDto: UpdateContractAttributeDto, user: IUser) {
-      const { id, ...data } = updateContractAttributeDto
-      if (id && !(await this.findOneById(id)))
-         throw new BadRequestException(RESPONSE_MESSAGES.CONTRACT_ATTRIBUTE_NOT_FOUND)
-      const updatedBy: IExecutor = { id: user.id, name: user.name, email: user.email, role: user.role }
-      const contractAttribute = await this.prismaService.client.contractAttribute.update({
-         where: { id },
-         data: { ...data, updatedBy }
+    return contractAttributes
+  }
+
+  // async findAllInBlockchainByContractId(contractId: string): Promise<IContractAttributeResponse[]> {
+  //    const contractAttributes = await this.prismaService.client.contractAttributeInBlockchain
+  //       .findMany({
+  //          where: { contractId },
+  //          include: { ContractAttributeValueInBlockchain: true },
+  //          orderBy: {
+  //             index: 'asc'
+  //          }
+  //       })
+  //       .then((contractAttributes) => this.commonService.convertToTypeContractAttributesResponse(contractAttributes))
+
+  //    return contractAttributes
+  // }
+
+  async findAllByTemplateId(templateContractId: string) {
+    const templateContract = await this.templateContractsService.findOneById(templateContractId)
+    const contractAttributes = await Promise.all(
+      templateContract.contractAttributes.map(async (item) => {
+        const contractAttribute = await this.prismaService.client.contractAttribute.findFirst({
+          where: { id: item },
+          include: { ContractAttributeValue: true }
+        })
+        return contractAttribute
       })
+    )
+    const result = await Promise.all(this.commonService.convertToTypeContractAttributesResponse(contractAttributes))
+    return result
+  }
 
-      return contractAttribute
-   }
+  async findOneById(id: string) {
+    return await this.prismaService.client.contractAttribute.findUnique({ where: { id } })
+  }
 
-   remove(id: number) {
-      return `This action removes a #${id} contractAttribute`
-   }
+  async findOne(payload: string) {
+    const contractAttribute = await this.prismaService.client.contractAttribute.findFirst({
+      where: { value: payload }
+    })
+    return contractAttribute
+  }
+
+  async update(updateContractAttributeDto: UpdateContractAttributeDto, user: IUser) {
+    const { id, ...data } = updateContractAttributeDto
+    if (id && !(await this.findOneById(id)))
+      throw new BadRequestException(RESPONSE_MESSAGES.CONTRACT_ATTRIBUTE_NOT_FOUND)
+    const updatedBy: IExecutor = { id: user.id, name: user.name, email: user.email, role: user.role }
+    const contractAttribute = await this.prismaService.client.contractAttribute.update({
+      where: { id },
+      data: { ...data, updatedBy }
+    })
+
+    return contractAttribute
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} contractAttribute`
+  }
 }
